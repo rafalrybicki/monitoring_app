05340d3fd07405a1cdf3861de99e18764e754384
Rafał Rybicki <rafarybicki@gmail.com>
2022-09-22 13:17:05 +0200

    Add order to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_073914) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_111529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -124,6 +124,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_073914) do
     t.datetime "updated_at", null: false
     t.date "date", null: false
     t.boolean "cancelled", default: false
+    t.integer "order", default: 1
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
